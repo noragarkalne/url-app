7a3f5d80a612128814a62abea24281c2e872cbc2
nora.kalva <nora.kalva@gmail.com>
2020-11-03 22:09:34 +0200

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FormGroup, Label, Input, Button, Table } from "reactstrap";
+import axios from "axios";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    http: {
+      id: null,
+      url: "",
+      short: null,
+    },
+    newUrlData: {
+      id: null,
+      url: "",
+      short: null,
+    },
+    shorturl: "",
+  };
+
+  addUrl() {
+    axios
+      .put("http://localhost:3030/api/put/url", this.state.newUrlData)
+      .then((response) => {
+        this.setState({ shorturl: response.data });
+      });
+  }
+  render() {
+    return (
+      <div className="App-container">
+        <FormGroup>
+          <Label for="url" className="App-name">
+            Url shortener
+          </Label>
+          <Input
+            id="url"
+            value={this.state.newUrlData.url}
+            onChange={(e) => {
+              let { newUrlData } = this.state;
+
+              newUrlData.url = e.target.value;
+
+              this.setState({ newUrlData });
+            }}
+            placeholder="enter your url"
+          />
+          <Button onClick={this.addUrl.bind(this)} className="App-button">
+            shorten url
+          </Button>
+        </FormGroup>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>ShortUrl</th>
+            </tr>
+          </thead>
+          <tbody itemType="url">
+            <a href={this.state.newUrlData.url}>{this.state.shorturl}</a>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
